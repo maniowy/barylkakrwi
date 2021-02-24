5bd81a2b85c70632e1a11f4d9516f7baa2767a00
Mateusz Burnicki <matburnicki@gmail.com>
2021-02-24 16:35:48 +0100

    css auto cleanup

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('css-byebye')({rulesToRemove: [
+     'hr', 'iframe', 'table', 'code', 'pre code',
+      /\.hero-video.*/, /\.has-background-.*/,
+      /\.hero\.is-(info|success|link|small|(full|half)height).*/,
+      /\.hero-(body|buttons).*/,
+      /\.columns\.is-(mobile|desktop|multiline|gapless|vcentered).*/,
+      /\.column\.is-[^h].*/,
+      /\.column\.is-half-.*/,
+      /\.is-flex.*/,
+      /.*is-medium.*/,
+      /.*subtitle.*/,
+      /.*is-(white|black|light|dark|warning|variable|primary).*/,
+      /\.button\.is-info.*/,
+      /\.modal-card.*/,
+      /\.field-(label|body).*/,
+      /.*has-icons-left.*/
+    ], map: false}),
+    require('postcss-discard-empty')
+  ]
+};
